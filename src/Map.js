fd1b05082bc921715e37c8c134927edc09c9189b
Gavin Rehkemper <gavin@gavinr.com>
2021-01-05 22:43:59 -0600

    Feature Layer and icon fix
    fix from https://github.com/PaulLeCam/react-leaflet/issues/453#issuecomment-731732137

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,11 @@
 // @refresh reset
 import { useEffect, useRef } from "react";
-import { Map as LeafletMap } from "leaflet";
-import { VERSION, BasemapLayer } from "esri-leaflet";
+import { Map as LeafletMap, Icon, Marker } from "leaflet";
+import { VERSION, BasemapLayer, FeatureLayer } from "esri-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 function Map() {
   const mapRef = useRef(null);
@@ -10,10 +14,34 @@ function Map() {
     // debugger;
     if (mapRef.current !== null) {
       console.log("esri-leaflet version", VERSION);
+
+      // The default icon does not work due to webpack issues
+      let DefaultIcon = new Icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow,
+        iconAnchor: [12, 41],
+        popupAnchor: [0, -41],
+      });
+      Marker.prototype.options.icon = DefaultIcon;
+
       const map = new LeafletMap(mapRef.current);
-      map.setView([53.35014, -6.266155], 10);
+      map.setView([53.35014, -6.266155], 8);
+
+      // Add a basemap
       const basemap = new BasemapLayer("Streets");
       map.addLayer(basemap);
+
+      // Add a Feature Layer
+      const fl = new FeatureLayer({
+        url:
+          "https://sampleserver6.arcgisonline.com/arcgis/rest/services/SampleWorldCities/MapServer/0",
+      }).addTo(map);
+
+      fl.bindPopup(function (layer) {
+        return `<p><strong>${
+          layer.feature.properties.CITY_NAME
+        }</strong><br /> Population: ${layer.feature.properties.POP.toLocaleString("en")}</p>`;
+      });
     }
   }, [mapRef]);
 
