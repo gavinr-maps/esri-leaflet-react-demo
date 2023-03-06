69a0cc36f0b2001628479b6cc06694d733a1f678
Gavin Rehkemper <grehkemper@esri.com>
2023-03-06 14:13:45 -0800

    types

---
+++ src/types.d.ts
@@ -0,0 +1,4 @@
+// It does not seem like the types in esri-leaflet-vector
+// is being picked up, so disable the types here:
+// https://github.com/Esri/esri-leaflet-vector/issues/154#issuecomment-1457087336
+declare module "esri-leaflet-vector";
